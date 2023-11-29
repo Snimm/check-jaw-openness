67d4903a52be01e9be56945f76ea6db9f0acd91c
Sonnet <rexzeo0@gmail.com>
2023-11-30 03:11:43 +0530

    readme added

---
+++ README.md
@@ -0,0 +1,61 @@
+# Open Jaw detection with OpenCV and MediaPipe
+
+This Python program utilizes OpenCV and MediaPipe to perform face analysis on images and videos. The main functionalities include detecting landmarks on the face, checking jaw openness, and providing visualizations based on user-defined parameters.
+
+## Installation
+
+Before running the program, make sure to install the required dependencies:
+
+```bash
+pip install opencv-python mediapipe numpy
+```
+
+## Usage
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/your-username/check-jaw-openness.git
+cd check-jaw-openness
+```
+
+2. Run the program:
+
+```bash
+python check-jaw-openness.py
+```
+
+3. Follow the on-screen prompts to choose the type of input (image or video), provide the file path, and customize the analysis parameters.
+
+4. The program will display the processed output, showcasing facial landmarks and indicating jaw openness.
+
+## Customization
+
+- **Threshold for Jaw Openness:** Users can input a custom threshold for jaw openness or use the default value.
+
+- **Drawing Style:** Choose a drawing style for facial landmarks visualization, including TESSELATION, CONTOURS, IRIS, or None for no additional visualization.
+
+## Examples
+
+### Video Processing
+
+```bash
+python check-jaw-openness.py
+Enter the type of input (image/video): video
+Enter the path to the input file: path/to/your/video.mp4
+Do you want to enter a custom threshold for jaw openness? (y/n): y
+Enter the threshold for jaw openness: 0.01
+Do you want to enter a custom drawing style? (y/n): y
+Enter the drawing style (TESSELATION/CONTOURS/IRIS): CONTOURS
+```
+
+### Image Processing
+
+```bash
+python check-jaw-openness.py
+Enter the type of input (image/video): image
+Enter the path to the input file: path/to/your/image.jpg
+Do you want to enter a custom threshold for jaw openness? (y/n): n
+Do you want to enter a custom drawing style? (y/n): y
+Enter the drawing style (TESSELATION/CONTOURS/IRIS): TESSELATION
+```
--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from draw import draw_landmarks_on_image, draw_image, DrawingStyle
 import numpy as np
 
 
-
-
-
-def process_frame(image: mp.Image,jaw_openness_threshold, drawing_style ) -> np.ndarray:
+def process_frame(image: mp.Image, jaw_openness_threshold, drawing_style) -> np.ndarray:
     """
     Process a single frame by checking jaw openness, drawing landmarks, and displaying the processed image.
 
@@ -16,24 +13,26 @@ def process_frame(image: mp.Image,jaw_openness_threshold, drawing_style ) -> np.
         image (mp.Image): Input image.
 
     Returns:
-        mp.Image: Processed image.
+        np.ndarray: Processed image.
     """
-
     detection_result = check_image_for_jaw_openness(image)
     image = image.numpy_view()
     image = draw_landmarks_on_image(image, detection_result, drawing_style=drawing_style)
-    
+
     # Get the openness of the jaw
     openness_of_jaw = detection_result.face_blendshapes[0][25].score
     processed_image = draw_image(openness_of_jaw, image=image, jaw_openness_threshold=jaw_openness_threshold)
     return processed_image
 
-def run_video(video_path: str, jaw_openness_threshold, drawing_style) -> None:
+
+def run_video(video_path: str, jaw_openness_threshold:float, drawing_style:DrawingStyle) -> None:
     """
     Run video processing using the analyze_image module.
 
     Args:
         video_path (str): The path to the input video file.
+        jaw_openness_threshold: The threshold for jaw openness.
+        drawing_style: The drawing style for landmarks (TESSELATION, CONTOURS, IRIS, or None).
     """
     cam = cv2.VideoCapture(video_path)
     while True:
@@ -51,14 +50,16 @@ def run_video(video_path: str, jaw_openness_threshold, drawing_style) -> None:
     cam.release()
     cv2.destroyAllWindows()
 
-def run_image(image: mp.Image, jaw_openness_threshold, drawing_style) -> None:
+
+def run_image(image: mp.Image, jaw_openness_threshold:float, drawing_style:DrawingStyle) -> None:
     """
     Run image processing using the analyze_image module and display the processed image.
 
     Args:
         image (mp.Image): Input image.
+        jaw_openness_threshold: The threshold for jaw openness.
+        drawing_style: The drawing style for landmarks (TESSELATION, CONTOURS, IRIS, or None).
     """
-    
     processed_image = process_frame(image, jaw_openness_threshold, drawing_style)
     processed_image = cv2.cvtColor(processed_image, cv2.COLOR_RGB2BGR)
     cv2.imshow("Image Window", processed_image)
@@ -71,52 +72,56 @@ def take_input() -> tuple[str, str, float, DrawingStyle]:
     Take input from the user.
 
     Returns:
-        Tuple[str, str, float, DrawingStyle]: A tuple containing the type of input, the path to the input, the threshold for jaw openness, and the drawing style.
+        Tuple[str, str, float, DrawingStyle]: A tuple containing the type of input, the path to the input,
+        the threshold for jaw openness, and the drawing style.
     """
-
-    print("press q to exit the window created by the program")
-    type = input("Enter the type of input (image/video): ")
+    type = input("Enter the type of input (image/video): ").lower()
+    if type not in ["image", "video"]:
+        print("Invalid type.")
+        exit()
 
     path = input("Enter the path to the input file: ")
+    
     custom_jaw_openness_threshold = input("Do you want to enter a custom threshold for jaw openness? (y/n): ").lower()
     if custom_jaw_openness_threshold == "y":
         jaw_openness_threshold = float(input("Enter the threshold for jaw openness: "))
-
     elif custom_jaw_openness_threshold == "n":
         jaw_openness_threshold = 0.007
-    
     else:
-        print("Invalid input")
+        print("Invalid input.")
         exit()
 
     custom_drawing_style = input("Do you want to enter a custom drawing style? (y/n): ").lower()
-
     if custom_drawing_style == "y":
         drawing_style = input("Enter the drawing style (TESSELATION/CONTOURS/IRIS): ").upper()
-    elif custom_drawing_style == "n":  
+    elif custom_drawing_style == "n":
         drawing_style = None
     else:
-        print("Invalid input")
+        print("Invalid input.")
         exit()
 
     if drawing_style is not None:
         drawing_style = DrawingStyle[drawing_style]
+
     return type, path, jaw_openness_threshold, drawing_style
 
-def main(type, path, jaw_openness_threshold = 0.007, drawing_style = None) -> None:
+
+def main(type, path, jaw_openness_threshold=0.006, drawing_style=None) -> None:
     """
     Main function to demonstrate image processing on both image and video.
+
+    Args:
+        type (str): The type of input (image or video).
+        path (str): The path to the input file.
+        jaw_openness_threshold (float): The threshold for jaw openness.
+        drawing_style (DrawingStyle): The drawing style for landmarks (TESSELATION, CONTOURS, IRIS, or None).
     """
     if type == "video":
-        video_path = path
-        run_video(video_path, jaw_openness_threshold, drawing_style)
-    
-    elif type =="image":
-        image_path = path
-        image = image_from_path(image_path)
+        run_video(path, jaw_openness_threshold, drawing_style)
+    elif type == "image":
+        image = image_from_path(path)
         run_image(image, jaw_openness_threshold, drawing_style)
-    else:
-        print("Invalid type")
+
 
 if __name__ == "__main__":
     type, path, jaw_openness_threshold, drawing_style = take_input()
